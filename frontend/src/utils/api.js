9fca4cfa76fbf61c93dd7cf906a1307eb082f521
diwakar733 <nallappagaridiwakar@gmail.com>
2026-03-03 22:11:50 +0530

    Updated production API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://interview-platform-7r64.onrender.com',
   headers: {
     "Content-Type": "application/json",
   },
